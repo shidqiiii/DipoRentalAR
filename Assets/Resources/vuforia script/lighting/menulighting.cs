d0087dc39476cda500f6eb861ccb89e34b37ce81
Muhammad Fadhli Hisyam <38355018+fadhlifadra@users.noreply.github.com>
2020-01-16 22:14:28 +0700

    paket 1 dan 2 lighting selesai
    lanjutkannn

---
+++ Assets/Resources/vuforia script/lighting/menulighting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menulighting : MonoBehaviour
+{
+    public void GoBack()
+    {
+        Application.LoadLevel("menu_lighting");
+    }
+
+    void Update()
+    {
+        // Make sure user is on Android platform
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            // Check if Back was pressed this frame
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                // Quit the application
+                Application.LoadLevel("menu_lighting");
+            }
+        }
+    }
+}
