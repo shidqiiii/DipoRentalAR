d4a95b92161000fee53cff8f18c2548e85247b4a
Muhammad Fadhli Hisyam <38355018+fadhlifadra@users.noreply.github.com>
2020-01-16 17:03:27 +0700

    ar layout jadi
    lanjutkan gan

---
+++ Assets/Resources/vuforia script/rigging/paketmini1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paketmini1 : MonoBehaviour
+{
+    public void GoBack()
+    {
+        Application.LoadLevel("menu_riggingstage");
+    }
+
+    void Update()
+    {
+        // Make sure user is on Android platform
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            // Check if Back was pressed this frame
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                // Quit the application
+                Application.LoadLevel("menu_riggingstage");
+            }
+        }
+    }
+}
