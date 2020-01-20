312e09c8ffae6fb596e604f548f464f0a732af8b
shidqiiii <shidqidaffa@gmail.com>
2020-01-20 21:19:20 +0700

    otw lagi retail

---
+++ Assets/Resources/vuforia script/retail/smokegun_navcontrol.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smokegun_navcontrol : MonoBehaviour
+{
+    public void GoBack()
+    {
+        Application.LoadLevel("menu_retail");
+    }
+	
+	public void GoPrev(){
+		 Application.LoadLevel("cajon");
+	}
+	
+	public void GoNext(){
+		 Application.LoadLevel("podium");
+	}
+
+    void Update()
+    {
+        // Make sure user is on Android platform
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            // Check if Back was pressed this frame
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                // Quit the application
+                Application.LoadLevel("menu_retail");
+            }
+        }
+    }
+}
